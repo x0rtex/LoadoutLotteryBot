5ce06ad70adfac477db3d35407c7dae2fc349a90
x0rtex <39656613+x0rtex@users.noreply.github.com>
2023-03-10 16:04:18 +0000

    fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import option
 
 import asyncio
 import os
@@ -12,10 +13,12 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-debug_guilds = [1052251792798404719]
+debug_guilds = [
+    1052251792798404719,
+    1052251792798404719
+]
 
 bot = commands.Bot(
-    intents=discord.Intents.all(),
     debug_guilds=debug_guilds,
 )
 
@@ -26,20 +29,29 @@ async def on_ready():
     print(f"Guilds: {len(bot.guilds)}")
 
 
-@bot.command(name="ping", description="Check the bot's latency.")
+# /ping
+@bot.slash_command(name="ping", description="Check the bot's latency.")
 @commands.cooldown(1, 5, commands.BucketType.user)
-async def ping(ctx: commands.Context):
-    await ctx.reply(f":hourglass: **Ping:** {round(bot.latency * 100, 2)} ms")
+async def ping(ctx: discord.ApplicationContext):
+    await ctx.respond(f":ping_pong: **Ping:** {round(bot.latency * 100, 2)} ms")
 
 
+# /roll
+@bot.slash_command(name="roll", description="Loadout Lottery!")
+@commands.cooldown(1, 20, commands.BucketType.user)
+@option("items", description="Would you like to roll between all items, or only meta items?", choices=["Standard", "Meta Only"])
+async def roll(ctx: discord.ApplicationContext, items: str):
+    await ctx.respond(f"Rolled! You chose {items} Loadout Lottery")
+
+
+# Application command error handler
 @bot.event
-async def on_application_command_error(ctx: commands.Context, error):
-    await ctx.reply(error, ephemeral=True)
+async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
     if isinstance(error, commands.CommandOnCooldown):
-        await asyncio.sleep(error.retry_after)
-        await ctx.message.delete()
+        await ctx.respond(
+            f":hourglass: **This command is currently on cooldown.** Try again in {round(error.retry_after, 1)}s.")
     else:
-        raise error
+        raise error  # Raise other errors so they aren't ignored
 
 
 def run_bot() -> None:
