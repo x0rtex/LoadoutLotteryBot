19230348ee9d7759ea6a343e5a5eff5eb7dc5232
x0rtex <39656613+x0rtex@users.noreply.github.com>
2023-03-12 07:21:56 +0000

    Add files via upload

---
+++ items/backpacks.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+
+class Backpack:
+    def __init__(
+            self,
+            name: str,
+            image_url: str,
+            trader: Literal["Flea", "Prapor", "Therapist", "Fence", "Skier", "Peacekeeper", "Mechanic", "Ragman", "Jaeger", None],
+            trader_level: Literal[0, 1, 2, 3, 4],
+            fir_only: bool,
+            quest_locked: bool,
+            meta: bool,
+    ):
+        self.name = name
+        self.image_url = image_url
+        self.trader = trader
+        self.trader_level = trader_level
+        self.fir_only = fir_only
+        self.quest_locked = quest_locked
+        self.meta = meta
+
+
+backpack_list = [
+
+    No_Backpack := Backpack(
+        name="No Armor",
+        image_url="",
+        trader=None,
+        trader_level=0,
+        fir_only=False,
+        quest_locked=False,
+        meta=False,
+    ),
+
+]
+
