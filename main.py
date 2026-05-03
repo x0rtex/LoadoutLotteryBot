b4eff2f4219f8bb01710a2f69ecf718d38fc782d
x0rtex <s00253368@atu.ie>
2026-05-03 17:25:23 +0100

    feat: add uv package manager support and update modules

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w"
 handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
 
+asyncio.set_event_loop(asyncio.new_event_loop())
 bot = commands.Bot(help_command=commands.DefaultHelpCommand())
 
 
