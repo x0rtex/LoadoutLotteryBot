e3ef012991366a5d9654967934c8d0f2b888094a
x0rtex <dev@x0rtex.xyz>
2026-05-15 23:21:05 +0100

    fix: move logging to logs folder

--- logger/logger.py
+++ logger/logger.py
@@ -1,11 +1,14 @@
 import logging
+import os
 
 from rich.logging import RichHandler
+
+os.makedirs("logs", exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO, format="%(message)s", handlers=[RichHandler(rich_tracebacks=True, show_path=False, markup=True)]
 )
 logger = logging.getLogger("discord")
-file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler = logging.FileHandler(filename="logs/discord.log", encoding="utf-8", mode="w")
 file_handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(file_handler)
